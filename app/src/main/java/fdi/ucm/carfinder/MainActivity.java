815cd9fb7d5b1f1daf41a9be852ba42b7f5c69ba
torresfel <josse995@hotmail.com>
2017-04-29 18:21:45 +0200

    Registro finiquitao

--- app/src/main/java/fdi/ucm/carfinder/MainActivity.java
+++ app/src/main/java/fdi/ucm/carfinder/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,17 +81,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.inicio) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.coches) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.ajustes) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.cerrarSesion) {
 
         }
 
